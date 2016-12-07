9a6fe97ee8e9a43801b98b9f266be86c860dc2a3
hatr4781 <ha.tran@colorado.edu>
2016-12-07 13:27:21 -0700

    Updated UART code for USART2, working

--- PoC/UART/main.c
+++ PoC/UART/main.c
@@ -1,13 +1,3 @@
-/**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
-  ******************************************************************************
-*/
-
 #include "stm32f4xx.h"
 #include "stm32f4xx_gpio.h"
 #include "stm32f4xx_rcc.h"
@@ -20,110 +10,99 @@
 #define LED_BLUE	GPIO_Pin_15
 #define BUTTON_pin 	0x01
 
-
 int toggleLED(uint16_t led){
 	GPIOD->ODR ^= led;
 	return 0;
 }
 
-int sendMessage(char* message){
-	int i = 0;
-	while(message[i] != '\0'){
-		while(!(USART1->SR & USART_FLAG_TXE));
-		USART1->DR = (uint16_t) message[i] & 0x01FF;
-		i++;
-	}
-	return 0;
+void sendChar(uint16_t c){
+	//while(!(USART1->SR & USART_FLAG_TXE));
+	while (!USART_GetFlagStatus(USART2, USART_FLAG_TXE));
+	USART_SendData(USART2, c);
+	//USART1->DR = (message & (uint16_t)0x01FF);
 }
 
-int sendChar(uint16_t message){
-	//while(!(USART1->SR & USART_FLAG_TXE));
-	USART1->DR = (message & (uint16_t)0x01FF);
-	return 0;
+void sendMessage(char* message){
+	while(*message){
+		sendChar(*message++);
+	}
+}
+
+uint16_t USART_GetChar(){
+	while(!USART_GetFlagStatus(USART2, USART_FLAG_RXNE));
+	return USART_ReceiveData(USART2);
 }
 
 int main(void)
 {
-	//Initialization
-		GPIO_InitTypeDef GPIO_InitDef;
+	GPIO_InitTypeDef GPIO_InitDef;
+	USART_InitTypeDef USART_InitStruct;
 
-		RCC->AHB1ENR |= RCC_AHB1Periph_GPIOD; //enable GPIOD clock
-		GPIO_InitDef.GPIO_Pin = LED_GREEN| LED_ORANGE | LED_RED | LED_BLUE;
-		GPIO_InitDef.GPIO_Mode = GPIO_Mode_OUT;
-		GPIO_InitDef.GPIO_OType = GPIO_OType_PP;
-		GPIO_InitDef.GPIO_PuPd = GPIO_PuPd_NOPULL;
-		GPIO_InitDef.GPIO_Speed = GPIO_Speed_100MHz;
-		GPIO_Init(GPIOD, &GPIO_InitDef);
+	//Initialization for LEDs
+	RCC->AHB1ENR |= RCC_AHB1Periph_GPIOD; //enable GPIOD clock
+	GPIO_InitDef.GPIO_Pin = LED_GREEN| LED_ORANGE | LED_RED | LED_BLUE;
+	GPIO_InitDef.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitDef.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitDef.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_InitDef.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOD, &GPIO_InitDef);
 
-//		//Initialization for user button
-//		RCC->AHB1ENR |= RCC_AHB1Periph_GPIOA; //enable GPIOA clock
-//		GPIO_InitDef.GPIO_Pin = BUTTON_pin;
-//		GPIO_InitDef.GPIO_Mode = GPIO_Mode_IN;
-//		GPIO_InitDef.GPIO_OType = GPIO_OType_PP;
-//		GPIO_InitDef.GPIO_PuPd = GPIO_PuPd_DOWN;
-//		GPIO_InitDef.GPIO_Speed = GPIO_Speed_2MHz;
-//		GPIO_Init(GPIOA, &GPIO_InitDef);
+	//GPIO Initialization for USART1
+	/* Enable GPIO clock */
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 
-		//GPIO Initialization for USART1
-		RCC-> AHB1ENR |= RCC_AHB1Periph_GPIOA;
-		GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1);
-		GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1);
-		GPIO_InitDef.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
-		GPIO_InitDef.GPIO_Mode = GPIO_Mode_AF;
-		GPIO_InitDef.GPIO_OType = GPIO_OType_PP;
-		GPIO_InitDef.GPIO_PuPd = GPIO_PuPd_UP;
-		GPIO_InitDef.GPIO_Speed = GPIO_Speed_100MHz;
-		GPIO_Init(GPIOA, &GPIO_InitDef);
+	/* Enable UART clock */
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
 
-		//USART1 Initialization
-		USART_InitTypeDef USART_InitStruct;
-		RCC-> APB2ENR |= RCC_APB2Periph_USART1;	//enable clock for USART1
-		USART1->CR1 |= USART_CR1_UE;
-		USART_InitStruct.USART_BaudRate = 9600;
-		USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-		USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
-		USART_InitStruct.USART_Parity = USART_Parity_No;
-		USART_InitStruct.USART_StopBits = USART_StopBits_1;
-		USART_InitStruct.USART_WordLength = USART_WordLength_8b;
-		USART_Init(USART1, &USART_InitStruct);
+	//Connect PA2 to USART2 Tx
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2);
+	//Connect PA3 to USART2 Rx
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART2);
 
+	//Configure USART2 Tx as alternate function
+	GPIO_InitDef.GPIO_Pin = GPIO_Pin_2;
+	GPIO_InitDef.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitDef.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitDef.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitDef.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitDef);
 
-		char color;
-		char message[20] = "hello world\n";
-		//char test = 'g';
-		uint16_t num = 3;
+	//Configure USART2 Rx as alternate function
+	GPIO_InitDef.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitDef.GPIO_Pin = GPIO_Pin_3;
+	GPIO_Init(GPIOA, &GPIO_InitDef);
 
-		while(1){
-			if(USART1->SR & USART_FLAG_RXNE){
-				color = (char)USART1->DR & (char)0x01FF;
-				if(color == 'o'){
-					toggleLED(LED_ORANGE);
+	USART_InitStruct.USART_BaudRate = 9600;
+	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+	USART_InitStruct.USART_Parity = USART_Parity_No;
+	USART_InitStruct.USART_StopBits = USART_StopBits_1;
+	USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+	USART_Init(USART2, &USART_InitStruct);
 
-				}
-				if(color == 'g'){
-					toggleLED(LED_GREEN);
-				}
-				if(color == 'r'){
-					toggleLED(LED_RED);
-				}
-				if(color == 'b'){
-					toggleLED(LED_BLUE);
-				}
-				if(color == 't'){
-					//sendChar(num);
-					sendMessage(message);
-				}
-			}
+	USART_Cmd(USART2, ENABLE);
+
+	uint16_t color;
+	while(1){
+		color = USART_GetChar();
+		//color = (char)USART1->DR & (char)0x01FF;
+		if(color == 'o'){
+			toggleLED(LED_ORANGE);
+
 		}
-
-//	while(1){
-//		if(GPIOA->IDR & BUTTON_pin){
-//			GPIOD->BSRRL = LED_GREEN| LED_ORANGE | LED_RED | LED_BLUE;
-//			//GPIO_SetBits(GPIOD, LED_GREEN| LED_ORANGE | LED_RED | LED_BLUE);
-//		}
-//		else{
-//			GPIOD->BSRRH = LED_GREEN| LED_ORANGE | LED_RED | LED_BLUE;
-//			//GPIO_ResetBits(GPIOD, LED_GREEN| LED_ORANGE | LED_RED | LED_BLUE);
-//		}
-//	}
+		if(color == 'g'){
+			toggleLED(LED_GREEN);
+		}
+		if(color == 'r'){
+			toggleLED(LED_RED);
+		}
+		if(color == 'b'){
+			toggleLED(LED_BLUE);
+		}
+		if(color == 't'){
+			//sendChar(num);
+			sendMessage("hello world!\n");
+		}
+	}
 }
+
